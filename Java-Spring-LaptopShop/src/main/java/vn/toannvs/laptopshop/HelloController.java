13885b7eb336effe3dbf39699ec42234734a098b
letrungviet2005 <vitprofb@gmail.com>
2025-06-27 13:58:05 +0700

    upload

--- Java-Spring-LaptopShop/src/main/java/vn/toannvs/laptopshop/HelloController.java
+++ Java-Spring-LaptopShop/src/main/java/vn/toannvs/laptopshop/HelloController.java
@@ -8,7 +8,7 @@ public class HelloController {
 
     @GetMapping("/")
     public String index() {
-        return "Hello, my name is Viet";
+        return "Hello, my name is Toan, I am a Java developer   ";
     }
 
     @GetMapping("/user")
