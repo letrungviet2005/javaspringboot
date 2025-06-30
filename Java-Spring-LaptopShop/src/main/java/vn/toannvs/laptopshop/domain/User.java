4272c6377f8a46a67a0a337b973a6a7df4ad56fe
letrungviet2005 <vitprofb@gmail.com>
2025-06-30 15:51:22 +0700

    new upload

--- Java-Spring-LaptopShop/src/main/java/vn/toannvs/laptopshop/domain/User.java
+++ Java-Spring-LaptopShop/src/main/java/vn/toannvs/laptopshop/domain/User.java
@@ -1,11 +1,32 @@
 package vn.toannvs.laptopshop.domain;
 
+import com.fasterxml.jackson.annotation.JsonIdentityInfo;
+import com.fasterxml.jackson.annotation.ObjectIdGenerators;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+@Entity
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
     private String email;
     private String password;
     private String fullname;
     private String phone;
     private String address;
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public long getId() {
+        return id;
+    }
 
     // Getters and setters
     public String getEmail() {
@@ -50,7 +71,7 @@ public class User {
 
     @Override
     public String toString() {
-        return "User [email=" + email + ", password=" + password + ", phone=" + phone
+        return "User [ Id =" + id + ",email=" + email + ", password=" + password + ", phone=" + phone
                 + ", fullname=" + fullname + ", address=" + address + "]";
     }
 }
