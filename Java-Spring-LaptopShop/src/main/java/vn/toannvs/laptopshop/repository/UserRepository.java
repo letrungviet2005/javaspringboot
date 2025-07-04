525eb93d7512807f91e884aad3f8e308646c4082
letrungviet2005 <vitprofb@gmail.com>
2025-07-04 16:57:58 +0700

    new upload

--- Java-Spring-LaptopShop/src/main/java/vn/toannvs/laptopshop/repository/UserRepository.java
+++ Java-Spring-LaptopShop/src/main/java/vn/toannvs/laptopshop/repository/UserRepository.java
@@ -3,10 +3,16 @@ package vn.toannvs.laptopshop.repository;
 import org.springframework.stereotype.Repository;
 import vn.toannvs.laptopshop.domain.User;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
     User save(User hoidanit);
+
+    List<User> findAll();
+
+    List<User> findAllByEmail(String email);
 
 }
