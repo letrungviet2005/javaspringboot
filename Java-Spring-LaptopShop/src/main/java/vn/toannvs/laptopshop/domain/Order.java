18ccb8d4db1c6e409c43d28baeed587751946236
letrungviet2005 <vitprofb@gmail.com>
2025-07-09 22:52:51 +0700

    new upload

--- Java-Spring-LaptopShop/src/main/java/vn/toannvs/laptopshop/domain/Order.java
+++ Java-Spring-LaptopShop/src/main/java/vn/toannvs/laptopshop/domain/Order.java
@@ -1,24 +1,23 @@
-package vn.toannvs.laptopshop.domain;
-import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
+// package vn.toannvs.laptopshop.domain;
+// import jakarta.persistence.Entity;
+// import jakarta.persistence.Table;
 
-@Entity
-@Table(name = "orders")
-public class Order {
+// @Entity
+// @Table(name = "orders")
+// public class Order {
 
-    private long id;
-    private double totalPrice;
-    public long getId() {
-        return id;
-    }
-    public void setId(long id) {
-        this.id = id;
-    }
-    public double getTotalPrice() {
-        return totalPrice;
-    }
-    public void setTotalPrice(double totalPrice) {
-        this.totalPrice = totalPrice;
-    }
-    
-}
+// private long id;
+// private double totalPrice;
+// public long getId() {
+// return id;
+// }
+// public void setId(long id) {
+// this.id = id;
+// }
+// public double getTotalPrice() {
+// return totalPrice;
+// }
+// public void setTotalPrice(double totalPrice) {
+// this.totalPrice = totalPrice;
+// }
+// }
