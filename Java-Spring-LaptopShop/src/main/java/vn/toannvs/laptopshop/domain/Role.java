18b14614b4db3bfd2eac10a32e0c5f4c90d54401
letrungviet2005 <vitprofb@gmail.com>
2025-07-12 16:53:06 +0700

    new upload

--- Java-Spring-LaptopShop/src/main/java/vn/toannvs/laptopshop/domain/Role.java
+++ Java-Spring-LaptopShop/src/main/java/vn/toannvs/laptopshop/domain/Role.java
@@ -6,39 +6,50 @@ import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
-import lombok.Data;
+import jakarta.persistence.OneToMany;
+import java.util.List;
 
 @Entity
 @Table(name = "roles")
 public class Role {
-    
+
     @Id
     @GeneratedValue(strategy = jakarta.persistence.GenerationType.IDENTITY)
     private Long id;
     private String name;
     private String description;
+
+    // Role ont to many users
+    @OneToMany
+    private List<User> users;
+
     public Long getId() {
         return id;
     }
+
     public void setId(Long id) {
         this.id = id;
     }
+
     public String getName() {
         return name;
     }
+
     public void setName(String name) {
         this.name = name;
     }
+
     public String getDescription() {
         return description;
     }
+
     public void setDescription(String description) {
         this.description = description;
     }
+
     @Override
     public String toString() {
         return "Role [id=" + id + ", name=" + name + ", description=" + description + "]";
     }
-    
 
 }
