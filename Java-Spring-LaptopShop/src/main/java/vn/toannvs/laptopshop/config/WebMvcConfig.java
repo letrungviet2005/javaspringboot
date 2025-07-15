a4828432b3defb5aa99248cd402ab2038621f560
letrungviet2005 <vitprofb@gmail.com>
2025-07-15 21:45:12 +0700

    new upload

--- Java-Spring-LaptopShop/src/main/java/vn/toannvs/laptopshop/config/WebMvcConfig.java
+++ Java-Spring-LaptopShop/src/main/java/vn/toannvs/laptopshop/config/WebMvcConfig.java
@@ -34,5 +34,7 @@ public class WebMvcConfig implements WebMvcConfigurer {
         registry.addResourceHandler("/js/**").addResourceLocations("/resources/js/");
         registry.addResourceHandler("/assets/**")
                 .addResourceLocations("/resources/assets/");
+        registry.addResourceHandler("/images/**")
+                .addResourceLocations("/resources/images/");
     }
 }
