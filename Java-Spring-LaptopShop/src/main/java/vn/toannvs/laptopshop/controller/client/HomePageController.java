97d104a42f2c476bbbf5ba14800f2e7190c7abbc
letrungviet2005 <vitprofb@gmail.com>
2025-07-20 18:35:44 +0700

    new upload

---
+++ Java-Spring-LaptopShop/src/main/java/vn/toannvs/laptopshop/controller/client/HomePageController.java
@@ -0,0 +1,14 @@
+package vn.toannvs.laptopshop.controller.client;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomePageController {
+
+    @GetMapping("/homepage")
+    public String homePage() {
+        return "client/homepage";
+    }
+
+}
